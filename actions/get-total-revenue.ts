10002381f42f073276d9612c1a64c6303f176308
yassinemontassar <sawthegamer70@gmail.com>
2023-11-25 00:27:28 +0100

    update total revenue functionality

--- actions/get-total-revenue.ts
+++ actions/get-total-revenue.ts
@@ -17,7 +17,7 @@ export const getTotalRevenue = async (storeId: string) => {
 
   const totalRevenue = paidOrders.reduce((total, order) => {
     const orderTotal = order.orderItems.reduce((orderSum, item) => {
-      return orderSum + item.product.price.toNumber();
+      return orderSum + (item.product.price.toNumber() * item.quantity  );
     }, 0);
     return total + orderTotal;
   }, 0);
